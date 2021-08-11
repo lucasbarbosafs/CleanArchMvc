fb8dad026298bdb073e3257677a3b3b7c49e5ab2
Lucas Barbosa de Freitas Silva <lucas.freitas@meta.com.br>
2021-08-10 23:19:53 -0300

    Adição das classes de configuração dos modelos, que serão utilizadas pelo ApplicationDbContext para configuração correta das entidades no banco de dados. A implementação do banco de dados será realizada através do modelo Code First com EntityFrameworkCore.

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(category => category.Id);
+            builder.Property(category => category.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios")
+            );
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(product => product.Id);
+
+            builder.Property(product => product.Name).HasMaxLength(100).IsRequired();
+            builder.Property(product => product.Description).HasMaxLength(200).IsRequired();
+            builder.Property(product => product.Price).HasPrecision(10, 2);
+
+            builder.HasOne(product => product.Category).WithMany(category => category.Products).HasForeignKey(product => product.CategoryId);
+        }
+    }
+}
