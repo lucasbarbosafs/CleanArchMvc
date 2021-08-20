9331a74df13b991008f00c534b23ae080776f28c
Lucas Barbosa de Freitas Silva <lucas.freitas@meta.com.br>
2021-08-20 16:46:29 -0300

    Adição dos métodos SeedRoles e SeedUsers na classe startup da camada WebUI e adição do middleware de autenticação na classe startup.
    Refatoração do método SeedRoles da classe SeedUserRoleInitial.
    
    Adição da verificação de autorização no CategoriesController e no ProductsController.

--- CleanArchMvc.Infra.Data/Identity/SeedUserRoleInitial.cs
+++ CleanArchMvc.Infra.Data/Identity/SeedUserRoleInitial.cs
@@ -67,7 +67,7 @@ namespace CleanArchMvc.Infra.Data.Identity
                 IdentityResult roleResult = _roleManager.CreateAsync(role).Result;
             }
 
-            if (!_roleManager.RoleExistsAsync("User").Result)
+            if (!_roleManager.RoleExistsAsync("Admin").Result)
             {
                 IdentityRole role = new IdentityRole();
                 role.Name = "Admin";
