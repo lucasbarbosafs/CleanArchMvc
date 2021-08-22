8233dffe730ec7bd541dee7d345cebac8c8e8010
Lucas Barbosa de Freitas Silva <lucas.freitas@meta.com.br>
2021-08-22 02:16:38 -0300

    Adição do controller TokenController que será utilizado para as operações relativas ao login e registro via JWT. Esse controller recebe uma injeção de dependência da interface IAuthenticate que fornece os métodos necessários para realização da autenticação.
    Foi implementado no controller o método Login que recebe uma requisição via Post e autentica o usuário de acordo com as suas credenciais.

---
+++ CleanArchMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchMvc.API.Models;
+using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authenticate;
+
+        public TokenController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate ?? throw new ArgumentNullException(nameof(authenticate));
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel login)
+        {
+            var result = await _authenticate.Authenticate(login.Email, login.Password);
+
+            if (result)
+            {
+                //return GenerateToken(login);
+                return Ok("Login Successfully");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
