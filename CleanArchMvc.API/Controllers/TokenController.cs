5c8a37c59b687a421fb6e4326ace9044e43a949f
Lucas Barbosa de Freitas Silva <lucas.freitas@meta.com.br>
2021-08-22 02:47:10 -0300

    Adição do método GenerateToken que irá criar o token para autenticação. Nesse método geramos as claims para o usuário, obtemos a chave privada através do método SymmetricSecuritKey, geramos a assinatura digital através do método SigningCredentials utilizando um algoritmo de segurança Hmac Sha 256, definimos o tempo de expiração do token, geramos o token através do objeto JwtSecurityToken e retornamos o DTO UserToken com as informações necessárias.
    Refatoração do método Login do TokenController para utilizar o método GenerateToken.

--- CleanArchMvc.API/Controllers/TokenController.cs
+++ CleanArchMvc.API/Controllers/TokenController.cs
@@ -1,7 +1,12 @@
 ﻿using CleanArchMvc.API.Models;
 using CleanArchMvc.Domain.Account;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.API.Controllers
@@ -11,10 +16,12 @@ namespace CleanArchMvc.API.Controllers
     public class TokenController : ControllerBase
     {
         private readonly IAuthenticate _authenticate;
+        private readonly IConfiguration _configuration;
 
-        public TokenController(IAuthenticate authenticate)
+        public TokenController(IAuthenticate authenticate, IConfiguration configuration)
         {
             _authenticate = authenticate ?? throw new ArgumentNullException(nameof(authenticate));
+            _configuration = configuration;
         }
 
         [HttpPost("LoginUser")]
@@ -24,14 +31,42 @@ namespace CleanArchMvc.API.Controllers
 
             if (result)
             {
-                //return GenerateToken(login);
-                return Ok("Login Successfully");
+                return GenerateToken(login);
             }
             else
             {
                 ModelState.AddModelError(string.Empty, "Invalid login attempt");
                 return BadRequest(ModelState);
             }
+        }
+
+        private UserToken GenerateToken(LoginModel login)
+        {
+            var claims = new[]
+            {
+                new Claim("email", login.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var privateKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                    issuer: _configuration["Jwt:Issuer"],
+                    audience: _configuration["Jwt:Audience"],
+                    claims: claims,
+                    expires: expiration,
+                    signingCredentials: credentials
+                );
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
         }
     }
 }
